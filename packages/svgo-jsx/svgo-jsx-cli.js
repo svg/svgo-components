93ebc97a04f4e7e5cdbfd8c0bfd735c61c16e018
Bogdan Chadkin <trysound@yandex.ru>
2021-07-29 13:14:41 +0300

    Avoid top-level await to support node 12

--- packages/svgo-jsx/svgo-jsx-cli.js
+++ packages/svgo-jsx/svgo-jsx-cli.js
@@ -44,50 +44,51 @@ const configFile = path.isAbsolute(rawConfigFile)
 const configDir = path.dirname(configFile);
 let count = 0;
 
-const cwd = process.cwd();
-const { config } = await import(configFile);
-if (config.inputDir == null) {
-  throw Error("inputDir string should be specified");
-}
-if (config.outputDir == null) {
-  throw Error("output string should be specified");
-}
+(async () => {
+  const { config } = await import(configFile);
+  if (config.inputDir == null) {
+    throw Error("inputDir string should be specified");
+  }
+  if (config.outputDir == null) {
+    throw Error("output string should be specified");
+  }
 
-const inputDir = path.join(configDir, config.inputDir);
-const outputDir = path.join(configDir, config.outputDir);
+  const inputDir = path.join(configDir, config.inputDir);
+  const outputDir = path.join(configDir, config.outputDir);
 
-const list = await fs.readdir(inputDir, { withFileTypes: true });
-await Promise.all(
-  list.map(async (dirent) => {
-    if (dirent.isFile()) {
-      count += 1;
-      const svgFile = path.join(inputDir, dirent.name);
-      const svg = await fs.readFile(svgFile, "utf-8");
-      const jsx = convertSvgToJsx({
-        file: path.relative(configDir, svgFile),
-        svg,
-        svgProps: config.svgProps || { "{...props}": null },
-        plugins: config.plugins || extendDefaultPlugins([]),
-      });
+  const list = await fs.readdir(inputDir, { withFileTypes: true });
+  await Promise.all(
+    list.map(async (dirent) => {
+      if (dirent.isFile()) {
+        count += 1;
+        const svgFile = path.join(inputDir, dirent.name);
+        const svg = await fs.readFile(svgFile, "utf-8");
+        const jsx = convertSvgToJsx({
+          file: path.relative(configDir, svgFile),
+          svg,
+          svgProps: config.svgProps || { "{...props}": null },
+          plugins: config.plugins || extendDefaultPlugins([]),
+        });
 
-      const template = config.template || defaultTemplate;
-      const transformFilename =
-        config.transformFilename || defaultTransformFilename;
-      const componentName = transformComponentName(dirent.name);
-      const jsxFilename = transformFilename(dirent.name);
-      const jsxFile = path.join(outputDir, jsxFilename);
-      const component = template({
-        sourceFile: path.relative(configDir, svgFile),
-        targetFile: path.relative(configDir, jsxFile),
-        componentName,
-        jsx,
-      });
-      await fs.mkdir(outputDir, { recursive: true });
-      await fs.writeFile(jsxFile, component);
-    }
-  })
-);
+        const template = config.template || defaultTemplate;
+        const transformFilename =
+          config.transformFilename || defaultTransformFilename;
+        const componentName = transformComponentName(dirent.name);
+        const jsxFilename = transformFilename(dirent.name);
+        const jsxFile = path.join(outputDir, jsxFilename);
+        const component = template({
+          sourceFile: path.relative(configDir, svgFile),
+          targetFile: path.relative(configDir, jsxFile),
+          componentName,
+          jsx,
+        });
+        await fs.mkdir(outputDir, { recursive: true });
+        await fs.writeFile(jsxFile, component);
+      }
+    })
+  );
 
-const end = process.hrtime.bigint();
+  const end = process.hrtime.bigint();
 
-console.info(`Compiled ${count} icons in ${(end - start) / BigInt(1e6)}ms`);
+  console.info(`Compiled ${count} icons in ${(end - start) / BigInt(1e6)}ms`);
+})().catch((error) => console.error(error));
